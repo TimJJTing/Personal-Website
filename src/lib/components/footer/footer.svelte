f1b68556c4835b71c70483d1aef3e40504f0e116
Jie-Ting Jiang <stu87282@gmail.com>
2024-10-29 02:25:52 +0800

    change footer style

--- src/lib/components/footer/footer.svelte
+++ src/lib/components/footer/footer.svelte
@@ -1,6 +1,7 @@
 <script>
-    // let year = new Date().getFullYear();
+	// let year = new Date().getFullYear();
 </script>
-<footer class="my-16 flex w-full justify-center">
+
+<footer class="my-16 flex w-full justify-center font-light text-neutral-400">
 	<span>Jie Ting Jiang</span>
 </footer>
