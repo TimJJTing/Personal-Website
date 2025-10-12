13b679f787358e006750f56ea946f3f15c258cfc
Jie-Ting Jiang <jieting.jiang@isi-hagenberg.at>
2025-10-13 01:55:24 +0800

    upgrade node and deps versions, fix mdsvex breaking changes

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,4 +1,11 @@
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 import remarkFootnotes from "remark-footnotes";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const path_to_layout = join(__dirname, "./src/lib/markdown-layouts");
 
 const config = {
 	extensions: ['.md', '.svx'],
@@ -7,10 +14,10 @@ const config = {
 		dashes: 'oldschool'
 	},
 	layout: {
-		links: './src/lib/markdown-layouts/links.svelte',
-		article: './src/lib/markdown-layouts/article.svelte',
-		work: './src/lib/markdown-layouts/work.svelte',
-		_: './src/lib/markdown-layouts/default.svelte'
+		links: path_to_layout + '/links.svelte',
+		article: path_to_layout + '/article.svelte',
+		work: path_to_layout + '/work.svelte',
+		_: path_to_layout + '/default.svelte'
 	},
 	remarkPlugins: [remarkFootnotes],
 	rehypePlugins: []
